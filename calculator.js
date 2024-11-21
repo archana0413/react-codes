b477baff65e2d355cd8ed4091b8dce8355dce3ad
archana phapale <archanaphapale8@gmail.com>
2024-11-21 16:58:53 +0530

    module- import and export

---
+++ calculator.js
@@ -0,0 +1,9 @@
+
+let sumData = (a,b)=> {
+    return a+b;
+}
+let minData = (a,b)=> {
+    return a-b;
+}
+export let cname="module-import and export"
+export {sumData,minData}
---
+++ index.js
@@ -0,0 +1,7 @@
+
+import {sumData as sum,minData, cname} from "./calculator.js";
+console.log(sum(31,30));
+console.log(minData(61,30));
+console.log(cname);
+
+
